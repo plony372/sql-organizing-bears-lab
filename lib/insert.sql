2a1e7fb403b97684ce02901d96341f0bc482e824
MenachemFuterfas <menachemfuterfas@gmail.com>
2021-03-03 10:10:55 -0500

    commit tree 667da13ca4ad4c1ba1e52d0ef61aa5357e34bb42

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr.Chocolate", 4, "F", "Blue", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 2, "F", "Blue", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 5, "F", "black", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("sergeant Brown", 5, "F", "black", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 7, "F", "brown", "aggressive", 2);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 6, "M", "brown", "aggressive", 2);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 5, "M", "brown", "calm", 1);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 4, "M", "brown", "aggressive", 1);
+
+-- INSERT INTO cats (name, age, breed) VALUES ("Hannah", 1, "Tabby");
